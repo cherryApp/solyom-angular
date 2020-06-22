dbd0d54a94648783d30637112f2773a034044bc1
unknown <j.cserko@gmail.com>
2020-06-22 15:46:49 +0200

    Feat: auto push.

--- components/src/app/service/bill.service.ts
+++ components/src/app/service/bill.service.ts
@@ -25,5 +25,14 @@ export class BillService {
   ];
 
   constructor() {
+    setInterval( () => {
+      this.list.push(
+        new Bill({
+          szlaszam: 'LUK-2020-21460', db: 1, afaKulcs: '27', fizHat: new Date(2020, 5, 25),
+          kiallDatum: new Date(), kibocsajto: 'Lukoil Hungary Man. Kft. Bp. XXII. Lukoil út 22.',
+          me: 'db', nOsszeg: 20000, teljDatum: new Date(), termek: 'ESZ-95', vevo: 'Nyugtás Vevő'
+        })
+      );
+    }, 5000);
   }
 }
